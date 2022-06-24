2684ba44b7fcb67b4b4045e08e2bbf992128a05c
Emzzy241 <emzzyoluwole@gmail.com>
2022-06-24 09:40:43 +0100

    Working with scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready( function(){
+//    var name = $("#animal").find("option:selected").text();
+
+if( $("#animals = .snake")){
+    $("#snakes").show();
+}
+else if( $("#animals = .turtle")){
+    $("#turtle").show();
+}
+else( $("animals = .insects")){
+    $("#insects").show();
+}
+
+
+
+});
